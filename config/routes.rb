686c79068ebe030239b8878355ae842610df9364
sakuow <yusaku1335@gmail.com>
2021-10-12 00:40:50 +0000

    5 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
   root to: 'homes#top'
   get '/home/about' => 'homes#about'
   devise_for :users
+
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
     resources :comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
+
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
